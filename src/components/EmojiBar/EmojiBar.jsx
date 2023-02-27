cbb90def25bf8b7cd75c0a9c40fcd8b6ed7e835b
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-02-27 23:23:12 +0800

    test: fix yml

--- src/components/EmojiBar/EmojiBar.jsx
+++ src/components/EmojiBar/EmojiBar.jsx
@@ -1,9 +1,9 @@
 import './EmojiBar.less'
-import Emoji1 from '/e1.gif'
-import Emoji2 from '/e2.gif'
-import Emoji3 from '/e3.gif'
-import Emoji4 from '/e4.gif'
-import Emoji5 from '/e5.gif'
+import Emoji1 from 'e1.gif'
+import Emoji2 from 'e2.gif'
+import Emoji3 from 'e3.gif'
+import Emoji4 from 'e4.gif'
+import Emoji5 from 'e5.gif'
 
 const emojis = [Emoji1, Emoji2, Emoji3, Emoji4, Emoji5];
 
